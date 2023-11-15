5cdb349181c3b2f7e32e13029de1dd7a17bd90ea
Imanol-Gaston <ikeralareki@gmail.com>
2023-11-15 15:30:38 +0100

    railway config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from os import environ
+
 
 class Config:
     pass
@@ -8,7 +10,12 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12341234@localhost:5432/sitework'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class ProductionConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development': DevelopmentConfig,
+    'production': ProductionConfig
 }
